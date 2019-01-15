86b74a38b3087b884704bda12f26a60577f49426
Ricky JW <rickywid@hotmail.com>
2019-01-14 20:11:54 -0500

    commit files

--- src/App.js
+++ src/App.js
@@ -1,27 +1,93 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import { Transition, animated } from 'react-spring'
+import { data, socialLink } from './data';
+
 
 class App extends Component {
-  render() {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedProject: 0,
+      showBio: false
+    }
+  }
+
+  selectProject = (index) => {
+    this.setState({ 
+      selectedProject: index 
+    });
+  }
+
+  renderSocialIcons = (social, index) => {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
+      <li>
+        <a href={social.icon !== 'fa-envelope' ? social.link : 'mailto:rickywid@hotmail.com'} className="profile__social-link">
+          <i className={`fa ${social.icon}`} aria-hidden="true"></i>
+        </a>            
+      </li>
+    )
+  }
+  renderProjectNav() {}
+  renderProjects = (project, index) => {
+    return (
+      <div className={`project-name ${this.state.selectedProject !== index ? 'hide' : ''}`}>
+        <img src={project.img} alt={project.name} />
+        <div className="project-detail">
+          <h3>{project.name}</h3>  
+          <ul>
+            {project.skills.map(skill => <li className="skill"><p className="skill-item">{skill}</p><span> / </span></li>)}
+          </ul>       
+          <p className="project-desc">
+            {project.description}
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+          <div className="button-group">
+            <a href={project.links.source} className="source">View Source</a>
+            <a href={project.links.demo}  className="demo">Demo</a>
+          </div>
+        </div>      
+      </div> 
+    )
+  }
+
+
+  render() {
+    return(
+      <div className="container">
+        <section className="profile animated bounceInDown">
+          <div className="profile-info">
+            <div className="profile-details">
+              <h2 className="profile-name">Ricky JW </h2>
+              <p>rickywid@hotmail.com</p>
+              <p className="profile-title">Front End Developer</p>
+            </div>
+            <ul className="profile-social">
+              {socialLink.map(this.renderSocialIcons)}
+            </ul>         
+          </div>
+        </section>
+        <section className="projects-nav animated slideInLeft">
+          <ul>
+            {data.map((project, index) => <li className={`${this.state.selectedProject === index ? 'active' : ''}`} onClick={this.selectProject.bind(this, index)}><p>{project.name}</p></li> )}
+            <li className={`nav-bio ${this.state.selectedProject === data.length + 1 ? 'active' : ''}`} onClick={this.selectProject.bind(this, data.length + 1)}><p>ABOUT</p></li>
+          </ul>
+        </section>
+        <section className="project animated bounceInUp">   
+          {this.state.selectedProject === data.length + 1 ? 
+            <div className="bio">
+            <h5>about me...</h5>
+            <p>I am a <span class="highlight">Web Developer</span> with around <span class="highlight">2-3 years</span> of professional experience primarily focusing on <span class="highlight">Front End Development</span>. My core skills include <span class="highlight">HTML</span>, <span class="highlight">CSS</span> and <span class="highlight">JavaScript</span> but I have also used a wide variety of languages, frameworks, libraries and tools.</p>
+            <p>When I’m working on personal projects I like to experiment with different libraries and frameworks. I have used <span class="highlight">ReactJS</span> in a lot of my personal projects and most recently I got to learn a little bit of <span class="highlight">TypeScript</span>. As a Front End Developer I also try to learn more about the back end so I’ve taken the time to learn more about <span class="highlight">NodeJS</span>, <span class="highlight">ExpressJS</span> and <span class="highlight">databases</span> and understand how it all connects with the client side.</p>
+            <p>Apart from web development I also enjoy learning new programming languages. Most recently I’ve began learning <span class="highlight">Python</span> to perform web scraping and creating automated scripts.</p>
+            <p>If you would like to get in touch, you can contact me via email at <span className="highlight">rickywid@hotmail.com</span></p>
+            </div>
+            :
+            data.map(this.renderProjects)
+          }
+        </section>
+      </div>  
+    )  
   }
 }
 
---
+++ src/data.js
@@ -0,0 +1,61 @@
+export const data = [
+	{
+		name: 'MENTRSHIP',
+		skills: ['HTML', 'SCSS', 'JAVASCRIPT', 'REACT', 'TYPESCRIPT'],
+		description: 'This was a collaboration project that included 4 other developers. Mentrship is a platform where mentors can provide mentorship to those wanting to learn a new skill. Working on a group project allowed me to learn more about Git and React. It also gave me an opportunity to learn TypeScript. I also learned a lot how to work in teams and how to communicate and share ideas effectively.',
+		img: 'https://i.ibb.co/jk9zZ4V/portfolio-mentrship.jpg',
+		links: {
+			source: 'https://github.com/projectunic0rn/mentorship-ui',
+			demo: 'http://mentrship.com'
+		}
+	},
+	{
+		name: 'WEATHER',
+		skills: ['HTML', 'SCSS', 'JAVASCRIPT', 'REACT', 'OPENWEATHERMAP API'],
+		description: 'I created a weather application where users can search for a location and receive the weather foreacast. My objective for this project was to improve my UI skills by being able to implement a clean simple layout to display information. For this project I tried to use various methods for displaying data information including icons, charts and a map.',
+		img: 'https://i.ibb.co/gR0vRP1/portfolio-weather.jpg',
+		links: {
+			source: 'https://github.com/rickywid/weather-v2',
+			demo: 'https://rickywid.github.io/weather-v2/'
+		}
+	},
+	{
+		name: 'REDDIT',
+		skills: ['HTML', 'SCSS', 'JAVASCRIPT', 'REACT', 'REDDIT API'],
+		description: 'I created a web app where users can see all of their favourite subreddits on a single page. I wanted to save myself a little bit of time by not having to navigate between individual subreddit pages. I added features where users can add, edit, remove subreddits from their saved list. Users can identify if a post is a video or image type and view related comments linked directly to Reddit\'s website.',
+		img: 'https://i.ibb.co/6sCMgqc/portfolio-reddit.jpg',
+		links: {
+			source: 'https://github.com/rickywid/reddit-v2',
+			demo: 'https://rickywid.github.io/reddit-v2/'
+		}
+	},
+	{
+		name: 'HANGMAN',
+		skills: ['HTML', 'CSS', 'JAVASCRIPT'],
+		description: 'I created this web game to help improve my JavaScript skills. I used this project as a way for me to learn more about JavaScript fundamentals including functions, variable scope, array and object methods and more. I also wanted to improve how to write more organized code by moving reusable code into functions and writing meaningful variable names.',
+		img: 'https://i.ibb.co/Kr68Zjj/portfolio-hangman.jpg',
+		links: {
+			source: 'https://github.com/rickywid/hangman_js/tree/gh-pages',
+			demo: 'https://rickywid.github.io/hangman_js/'
+		}
+	}		
+]
+
+export const socialLink = [
+  {
+    icon: 'fa-github',
+    link: 'http://github.com/rickywid'
+  },
+  {
+    icon: 'fa-codepen',
+    link: 'http://codepen.io/rickyjw'
+  },
+  {
+    icon: 'fa-linkedin-square',
+    link: 'https://www.linkedin.com/in/ricky-widjaja-791059b6'
+  },
+  {
+    icon: 'fa-envelope',
+    link: 'mailto:rickywid@hotmail.com'
+  }
+]
