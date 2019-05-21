6c8019a7886dd6ef5b6b7303e7dcc57cf4b08005
rickyjw <rickywid@hotmail.com>
2019-05-21 14:50:18 -0400

    add space

--- src/App.js
+++ src/App.js
@@ -129,7 +129,7 @@ class App extends Component {
                 </ul>
                 <p>
                   If you would like to get in touch, you can contact me via email at
-                  <span className="highlight">rickywid@hotmail.com</span>
+                  <span className="highlight"> rickywid@hotmail.com</span>
                 </p>
               </div>
             )
