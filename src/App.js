baead6c6869e60c5f9df6ea2a3e1ddf0e913cda3
rickyjw <rickywid@hotmail.com>
2019-05-20 22:31:15 -0400

    add eslint. fix eslint errors, adjust padding in mobile view for bio section

--- src/App.js
+++ src/App.js
@@ -4,56 +4,63 @@ import { data, socialLink } from './data';
 
 
 class App extends Component {
-
   constructor(props) {
     super(props);
 
     this.state = {
       selectedProject: 0,
-      showBio: false
-    }
+    };
   }
 
   selectProject = (index) => {
-    this.setState({ 
-      selectedProject: index 
+    console.log(index)
+    this.setState({
+      selectedProject: index,
     });
   }
 
-  renderSocialIcons = (social, index) => {
-    return (
-      <li>
-        <a href={social.icon !== 'fa-envelope' ? social.link : 'mailto:rickywid@hotmail.com'} className="profile__social-link" rel="noopener noreferrer" target="_blank">
-          <i className={`fa ${social.icon}`} aria-hidden="true"></i>
-        </a>            
-      </li>
-    )
-  }
-  renderProjectNav() {}
+  renderSocialIcons = social => (
+    <li>
+      <a href={social.icon !== 'fa-envelope' ? social.link : 'mailto:rickywid@hotmail.com'} className="profile__social-link" rel="noopener noreferrer" target="_blank">
+        <i className={`fa ${social.icon}`} aria-hidden="true" />
+      </a>
+    </li>
+  )
+
   renderProjects = (project, index) => {
+    const { selectedProject } = this.state;
+
     return (
-      <div className={`project-name ${this.state.selectedProject !== index ? 'hide' : ''}`}>
+      <div className={`project-name ${selectedProject !== index ? 'hide' : ''}`}>
         <img src={project.img} alt={project.name} />
         <div className="project-detail">
-          <h3>{project.name}</h3>  
+          <h3>{project.name}</h3>
           <ul>
-            {project.skills.map(skill => <li className="skill"><p className="skill-item">{skill}</p><span> / </span></li>)}
-          </ul>       
+            {project.skills.map(skill => (
+              <li className="skill">
+                <p className="skill-item">{skill}</p>
+                <span> / </span>
+              </li>
+            ))
+            }
+          </ul>
           <p className="project-desc">
             {project.description}
           </p>
           <div className="button-group">
             <a href={project.links.source} className="source" rel="noopener noreferrer" target="_blank">View Source</a>
-            <a href={project.links.demo}  className="demo" rel="noopener noreferrer" target="_blank">Visit Site</a>
+            <a href={project.links.demo} className="demo" rel="noopener noreferrer" target="_blank">Visit Site</a>
           </div>
-        </div>      
-      </div> 
-    )
+        </div>
+      </div>
+    );
   }
 
 
   render() {
-    return(
+    const { selectedProject } = this.state;
+
+    return (
       <div className="container">
         <section className="profile animated bounceInDown">
           <div className="profile-info">
@@ -64,36 +71,73 @@ class App extends Component {
             </div>
             <ul className="profile-social">
               {socialLink.map(this.renderSocialIcons)}
-            </ul>         
+            </ul>
           </div>
         </section>
         <section className="projects-nav animated slideInLeft">
           <ul>
-            {data.map((project, index) => <li className={`${this.state.selectedProject === index ? 'active' : ''}`} onClick={this.selectProject.bind(this, index)}><p>{project.name}</p></li> )}
-            <li className={`nav-bio ${this.state.selectedProject === data.length + 1 ? 'active' : ''}`} onClick={this.selectProject.bind(this, data.length + 1)}><p>ABOUT</p></li>
+            {data.map((project, index) => <li><button type="button" className={`${selectedProject === index ? 'active' : ''}`} onKeyDown={this.selectProject.bind(this, index)} onClick={this.selectProject.bind(this, index)}>{project.name}</button></li>)}
+            <li><button type="button" className={`nav-bio ${selectedProject === data.length + 1 ? 'active' : ''}`} onClick={this.selectProject.bind(this, data.length + 1)}><p>ABOUT</p></button></li>
           </ul>
         </section>
-        <section className="project animated bounceInUp">   
-          {this.state.selectedProject === data.length + 1 ? 
-            <div className="bio">
-            <p>I am a <span className="highlight">Web Developer</span> with about <span className="highlight">2-3 years</span> of professional experience focusing on <span className="highlight">Front End Development</span>. My main responsibilities as a Front End Developer involves translating mockup designs, creating responsive layouts and building single page applications using <span className="highlight">JavaScript</span> frameworks such as <span className="highlight">React</span>. As a developer I am continuously learning, building and experimenting with different tools and technologies.</p>
-            <ul className="bio-skills">
-              <li><span className="highlight">Languages</span>: HTML, CSS, JavaScript/ES6, Python</li>
-              <li><span className="highlight">Frameworks</span>: React, Angular, Vue, Node/Express, Jquery, MVC</li>
-              <li><span className="highlight">Databases</span>: Postgres, MongoDB</li>
-              <li><span className="highlight">Deployment</span>: CircleCI, Heroku</li>
-              <li><span className="highlight">Source Control</span>: Github</li>
-              <li><span className="highlight">Test Automation</span>: Selenium</li>
-              <li><span className="highlight">Operating Systems</span>: Linux, Windows</li>
-            </ul>
-            <p>If you would like to get in touch, you can contact me via email at <span className="highlight">rickywid@hotmail.com</span></p>
-            </div>
-            :
-            data.map(this.renderProjects)
+        <section className="project animated bounceInUp">
+          {selectedProject === data.length + 1
+            ? (
+              <div className="bio">
+                <p>
+                  I am a
+                  <span className="highlight"> Web Developer </span>
+                  with about
+                  <span className="highlight"> 2-3 years </span>
+                  of professional experience focusing on
+                  <span className="highlight"> Front End Development</span>
+                  . My main responsibilities as a Front End Developer involves translating mockup designs, creating responsive layouts and building single page applications using
+                  <span className="highlight"> JavaScript </span>
+                  frameworks such as
+                  <span className="highlight"> React</span>
+                  . As a developer I am continuously learning, building and experimenting with different tools and technologies.
+                </p>
+                <ul className="bio-skills">
+                  <li>
+                    <span className="highlight">Languages: </span>
+                     HTML, CSS, JavaScript/ES6, Python
+                  </li>
+                  <li>
+                    <span className="highlight">Frameworks: </span>
+                    React, Angular, Vue, Node/Express, Jquery, MVC
+                  </li>
+                  <li>
+                    <span className="highlight">Databases: </span>
+                    Postgres, MongoDB
+                  </li>
+                  <li>
+                    <span className="highlight">Deployment: </span>
+                    CircleCI, Heroku
+                  </li>
+                  <li>
+                    <span className="highlight">Source Control: </span>
+                    Github
+                  </li>
+                  <li>
+                    <span className="highlight">Test Automation: </span>
+                    Selenium
+                  </li>
+                  <li>
+                    <span className="highlight">Operating Systems: </span>
+                    Linux, Windows
+                  </li>
+                </ul>
+                <p>
+                  If you would like to get in touch, you can contact me via email at
+                  <span className="highlight">rickywid@hotmail.com</span>
+                </p>
+              </div>
+            )
+            : data.map(this.renderProjects)
           }
         </section>
-      </div>  
-    )  
+      </div>
+    );
   }
 }
 
