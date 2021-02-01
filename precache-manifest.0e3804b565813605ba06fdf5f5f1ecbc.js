b2ad176acaea773a8640956cb113685f6632850f
rickywid <rickywid@hotmail.com>
2021-02-01 15:06:05 -0500

    Updates

---
+++ precache-manifest.0e3804b565813605ba06fdf5f5f1ecbc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8de16ee93c9a6bbe9228",
+    "url": "/portfolio/static/css/main.2a9b2acc.chunk.css"
+  },
+  {
+    "revision": "8de16ee93c9a6bbe9228",
+    "url": "/portfolio/static/js/main.8de16ee9.chunk.js"
+  },
+  {
+    "revision": "604dacc94ff142ca2b4a",
+    "url": "/portfolio/static/js/1.604dacc9.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "a5ca4a6b32d03ba8b2c58234d679eb66",
+    "url": "/portfolio/index.html"
+  }
+];
